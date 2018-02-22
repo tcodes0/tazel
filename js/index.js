958b617e47dacff794c2967d88404cf94cb55de3
Raphael Thomazella <1357rtr@gmail.com>
2018-02-21 21:45:54 -0300

    index.js - added () to toggle the guide

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
-let arts = [...document.querySelectorAll('article')]
-arts.forEach(art => art.innerText === '' ? art.classList.toString() : undefined)
+// let arts = [...document.querySelectorAll('article')]
+// arts.forEach(art => art.innerText === '' ? art.classList.toString() : undefined)
+// let lis = [... document.querySelectorAll('.link-container')]
+// lis.forEach(li => li.style.border = '1px solid white')
+const toggleGuide = () => {
+  let guide = document.querySelector('.guide')
+  if (guide.style.display === "") {
+    guide.style.display = 'block'
+  } else {
+    guide.style.display = ''
+  }
+}
