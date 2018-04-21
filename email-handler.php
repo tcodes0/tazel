186d3079929d5ce57979a36894fae87a1703fbb3
Raphael Thomazella <1357rtr@gmail.com>
2018-04-21 13:36:02 -0300

    +email php script

---
+++ email-handler.php
@@ -0,0 +1,25 @@
+<?php
+$errors = '';
+$myemail = 'tazel@tazel.website';//<-----Put Your email address here.
+if(empty($_POST['name'])  ||
+   empty($_POST['email']) ||
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+
+if( empty($errors))
+{
+$to = $myemail;
+$email_subject = "$name visited tazel home";
+$email_body = "$email_address\n\n$message";
+$headers = "From: $myemail\n";
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+//redirect to the 'thank you' page
+header('Location: about.html');
+}
+?>
